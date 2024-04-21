366258993052da3ae45e7b3c6bdd6950e87fe215
ameeshagireesh <ameesha.gireeshkm@gmail.com>
2024-04-21 22:51:10 +0530

    fix: footer fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,45 @@
+import { HiOutlineMail } from "react-icons/hi";
+import { BsTelephone } from "react-icons/bs";
+import { FaLinkedinIn } from "react-icons/fa";
+
 const Footer = () => {
     return (
-        <div className="flex-col space-y-10 bg-black text-white p-5">
-            <div className="font-normal text-4xl text-center p-5">monter</div>
-            <div className="flex space-x-10">
-                <div className="flex-col w-1/3 font-light text-base">
-                    <p className="font-medium text-2xl">Newsletter</p>
-                    <p>Subscribe to our exclusive newsletter to receive regular updates and discover invaluable business insights</p>
+        <div className="flex-col space-y-10 bg-black text-white p-10">
+            <div className="flex space-x-10 m-10">
+                <div className="flex-col w-1/2 font-light text-base m-5 space-y-3">
+                    <p className="font-medium text-2xl">
+                        Newsletter
+                    </p>
+                    <p className="font-light text-sm text-slate-300">
+                        Subscribe to our exclusive newsletter to receive regular updates and discover invaluable business insights.
+                    </p>
                 </div>
-                <div className="flex-col w-1/3 font-light text-base">
-                    <p className="font-medium text-2xl">Contact Us</p>
-                    <p>hello@monter.one</p>
-                    <p>+91 9819476544</p>
+                <div className="flex-col w-1/2 font-light text-base m-5 space-y-3">
+                    <p className="font-medium text-2xl">
+                        Contact Us
+                    </p>
+                    <div className="flex space-x-3">
+                        <a href="mailto:hello@monter.one">
+                            <HiOutlineMail size={20} />
+                        </a>
+                        <p className="font-light text-sm text-slate-300">
+                            hello@monter.one
+                        </p>
+                    </div>
+                    <div className="flex space-x-3">
+                        <a href="tel:+919819476544">
+                            <BsTelephone size={20} />
+                        </a>
+                        <p className="font-light text-sm text-slate-300">
+                            +91 9819476544
+                        </p>
+                    </div>
                 </div>
-                <div className="flex-col w-1/3 font-light text-base">
-                    <p className="font-medium text-2xl">Products</p>
-                    <p>Investor - Company matchmaking</p>
-                    <p>Business Valuation for Companies</p>
-                    <p>Funding Readiness Index (FRI)</p>
-                    <p>Consulting & Services</p>
-                </div>
+            </div>
+            <div className="flex justify-center ">
+                <a href="https://www.linkedin.com/company/monter-one/">
+                    <FaLinkedinIn size={20} />
+                </a>
             </div>
         </div>
     )
